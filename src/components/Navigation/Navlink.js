c39a16ced1741618969ff570a7e8b46f93d88ceb
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-19 01:21:25 +0300

    Add components folder

---
+++ src/components/Navigation/Navlink.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+
+
+function Navlink() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <div>
+            <img src='./planet.png' />
+            <h1>Space Traveller' Hub</h1>
+          </div>
+          <div>
+          </div>
+        </nav>
+      </header>
+    </div>
+  )
+}
+
+export default Navlink
