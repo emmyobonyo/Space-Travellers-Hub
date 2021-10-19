8556d2ea68bc008ee1fce1f54c364a5957fcea75
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-10-19 09:57:29 +0100

    Create new route and empty view component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './App.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
