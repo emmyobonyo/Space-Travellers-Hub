9b2f50dbfa2b804701ad60a502e787253ea6f6e6
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-21 23:13:53 +0300

    Add Cancel button

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchRockets } from '../redux/Rockets/rockets';
+import { fetchRockets, joinRocket, cancelRocket } from '../redux/Rockets/rockets';
 
 function Rockets() {
   const rockets = useSelector((state) => state.rocketState);
@@ -9,6 +9,14 @@ function Rockets() {
     dispatch(fetchRockets());
   }, []);
 
+  const joinRocketButton = (payload) => {
+    dispatch(joinRocket(rockets, payload));
+  };
+
+  const cancelRocketButton = (payload) => {
+    dispatch(cancelRocket(rockets, payload));
+  };
+
   return (
     <div>
       { rockets.map((rocket) => (
@@ -16,8 +24,16 @@ function Rockets() {
           <img alt="rocket" src={rocket.image} className="rocket-image" />
           <div className="rocket-information">
             <h2 className="rocket-information-elements">{ rocket.name }</h2>
+            { rocket.reserved && (
+              <span>Reserved</span>
+            ) }
             <p className="rocket-information-elements">{ rocket.description }</p>
-            <button type="button" className="rocket-information-elements">Reserve Rocket</button>
+            { !rocket.reserved && (
+              <button type="button" className="rocket-information-elements button" onClick={() => { joinRocketButton(rocket.id); }}>Reserve Rocket</button>
+            ) }
+            { rocket.reserved && (
+              <button type="button" className="rocket-information-elements cancel-button" onClick={() => { cancelRocketButton(rocket.id); }}>Cancel Reservation</button>
+            ) }
           </div>
         </div>
       )) }
