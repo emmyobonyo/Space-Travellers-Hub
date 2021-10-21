47ded4426bc485bbb0fd2bde29f0bc7a58fe8340
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2021-10-21 18:22:40 +0300

    display rockets

--- src/pages/Rockets.js
+++ src/pages/Rockets.js
@@ -1,18 +1,20 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchRockets } from '../redux/Rockets/rockets';
 
 function Rockets() {
+  const rockets = useSelector((state) => state.rocketState);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(fetchRockets());
   }, []);
 
   return (
-    <>
-      <p>Hello World</p>
-      <p>This is the Rockets page</p>
-    </>
+    <div>
+      { rockets.map((rocket) => (
+        <p key={rocket.id}>{ rocket.description }</p>
+      )) }
+    </div>
   );
 }
 
