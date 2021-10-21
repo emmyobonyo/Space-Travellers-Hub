226bdea22730718fb9a13dbfdc59badb52162ffc
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-10-21 18:03:18 +0100

    Add leave-missions functionality

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,13 +1,19 @@
 const FETCH_MISSIONS = 'react-spacex/missions/FETCH_MISSIONS';
 const JOIN_MISSIONS = 'react-spacex/missions/JOIN_MISSIONS';
+const LEAVE_MISSIONS = 'react-spacex/missions/LEAVE_MISSIONS';
 
 export const fetchMissions = (missions) => ({
   type: FETCH_MISSIONS,
-  missions,
+  payloads: missions,
 });
 
 export const joinMission = (missions) => ({
   type: JOIN_MISSIONS,
+  payloads: missions,
+});
+
+export const leaveMission = (missions) => ({
+  type: LEAVE_MISSIONS,
   missions,
 });
 
@@ -49,6 +55,17 @@ const missionsReducer = (state = initialState, action) => {
             return { ...mission };
           }
           return { ...mission, reserved: true };
+        }),
+      };
+
+    case LEAVE_MISSIONS:
+      return {
+        ...state,
+        newMissions: state.newMissions.map((mission) => {
+          if (mission.mission_id !== action.id) {
+            return { ...mission };
+          }
+          return { ...mission, reserved: false };
         }),
       };
     default:
