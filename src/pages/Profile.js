ebae9db88f3385013b32e99ecde45fabee07556d
emmanuellekamwa <61285247+emmanuellekamwa@users.noreply.github.com>
2021-10-22 22:43:25 +0100

    Add some styling to profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -2,17 +2,44 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-function Profile() {
+const Profile = () => {
+  const missions = useSelector((state) => state.missionsReducer.newMissions);
   const rockets = useSelector((state) => state.rocketState);
   const reservedRockets = rockets.filter((rocket) => rocket.reserved);
+
+  let filteredMissions;
+
+  if (missions) {
+    filteredMissions = missions.filter((mission) => mission.reserved);
+  }
+
   return (
-    <div className="profile">
-      <h1>My Rockets</h1>
-      { reservedRockets.map((rocket) => (
-        <h4 className="profile-rockets" key={rocket.id}>{ rocket.name }</h4>
-      )) }
-    </div>
+    <main>
+      <div className="profile__missions">
+        <h1 className="profile__h1">My Missions</h1>
+        <ul className="profile__ul">
+          {filteredMissions && filteredMissions.map((mission) => {
+            const { name, id } = mission;
+            return (
+              <li
+                key={id}
+                className="my-list"
+              >
+                {name}
+              </li>
+            );
+          })}
+
+        </ul>
+      </div>
+      <div className="profile">
+        <h1>My Rockets</h1>
+        { reservedRockets.map((rocket) => (
+          <h4 className="profile-rockets" key={rocket.id}>{ rocket.name }</h4>
+        )) }
+      </div>
+    </main>
   );
-}
+};
 
 export default Profile;
